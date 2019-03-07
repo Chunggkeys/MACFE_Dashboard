f5821c1cac2001b28a40e8e0a4bf510f617d6f72
Lawrence Chung <chungl1@mcmaster.ca>
2019-03-06 19:12:05 -0500

    slight code cleanup in guiModel

--- guiModel.py
+++ guiModel.py
@@ -9,34 +9,34 @@ class guiModel(QObject):
 	def __init__(self,mTemp,speed,bLevel,bTemp,mTorque,shutdown):
 		
 		super().__init__()
-		self.listValues = []
+		#self.listValues = []
 		self.mTemp = mTemp; self.speed = speed 
 		self.bLevel = bLevel; self.shutdown = shutdown
 		self.bTemp = bTemp; self.mTorque = mTorque
 
 	def setBatteryLevel(self, bLevel):
 		self.bLevel = bLevel
-		self.listValues.append(self.bLevel)
+		#self.listValues.append(self.bLevel)
 
 	def setBatteryTemperature(self, bTemp):
 		self.bTemp = bTemp
-		self.listValues.append(self.bTemp)
+		#self.listValues.append(self.bTemp)
 
 	def setMotorTemperature(self, mTemp):
 		self.mTemp = mTemp
-		self.listValues.append(self.mTemp)
+		#self.listValues.append(self.mTemp)
 	
 	def setSpeed(self, speed):
 		self.speed = speed
-		self.listValues.append(self.speed)
+		#self.listValues.append(self.speed)
 		
 	def setMotorTorque(self, mTorque):
 		self.mTorque = mTorque
-		self.listValues.append(self.mTorque)
+		#self.listValues.append(self.mTorque)
 		
 	def setShutdown(self, shutdown):
 		self.shutdown = shutdown
-		self.listValues.append(self.shutdown)
+		#self.listValues.append(self.shutdown)
 		#self.updateSignal.emit(self.listValues)
 
 	def getMotorTemperature(self):
