4fdc2343616446b6acc570f7c90dd806849118b7
Lawrence Chung <chungl1@mcmaster.ca>
2019-05-26 15:45:25 -0400

    Changed CAN message format

--- PiCANInterface.py
+++ PiCANInterface.py
@@ -23,8 +23,8 @@ print('\n\rCAN Rx test')
 print('Bring up CAN0....')
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
 time.sleep(0.1)
-dashboardID = 502
-filter = [{"can_id": 0x502, "can_mask": 0x7FF, "extended": False}]
+temperatureID = 502; vehicleID = 503
+filter = [{"can_id": 0x502, "can_mask": 0x7FF, "extended": False},{"can_id": 0x503, "can_mask": 0x7FF, "extended": False}]
 
 try:
 	bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
@@ -37,61 +37,40 @@ print('Ready')
 prevTime = 0;
 
 try:
-	while True:
-		message = bus.recv()	# Wait until a message is received.
+    while True:
+        message = bus.recv()	# Wait until a message is received.
 		
-		c = '{0:f} {1:x} {2:x} '.format(message.timestamp - prevTime, message.arbitration_id, message.dlc)
-		s=''
-		prevTime = message.timestamp
-		for i in range(message.dlc ):
-			s +=  '{0:x} '.format(message.data[i])
+        c = '{0:f} {1:x} {2:x} '.format(message.timestamp - prevTime, message.arbitration_id, message.dlc)
+        s=''
+        prevTime = message.timestamp
+        for i in range(message.dlc):
+            s +=  '{0:x} '.format(message.data[i])
 		
-		if int(hex(message.arbitration_id)[2:]) == dashboardID:
-                    # Do stuff with dash values
-                    ## message.data[0-3] are motor temperatures (LF,LR,RF,RR), respectively
-                    ## message.data[4] is vehicle speed
-                    leftFrontMotorTemperature = message.data[0] - 20
-                    leftRearMotorTemperature = message.data[1] - 20
-                    rightFrontMotorTemperature = message.data[2] - 20
-                    rightRearMotorTemperature = message.data[3] - 20
-                    speed = message.data[4]
-##                    if message.data[5] >= 128:
-##                        soc = message.data[5] - 128
-##                    else:
-##                        soc = message.data[5]
-                    tempStartup = message.data[5] >> 7
-                    tempStartup2 = message.data[6] << 1
-                    tempStartup2 = tempStartup2 & 2
-                    startupStatus = tempStartup | tempStartup2
-                    
-                    soc = message.data[5] & 255
-##                    if message.data[5] >= 128 and message.data[6] % 2 != 0:
-##                        startupStatus = 3
-##                    elif message.data[5] < 128 and message.data[6] % 2 != 0:
-##                        startupStatus = 2
-##                    elif message.data[5] >= 128 and message.data[6] % 2 == 0:
-##                        startupStatus = 1
-##                    else:
-##                        startupStatus = 0
-                    maxPower = message.data[6] >> 1
-                    vcuError = message.data[7]
-                    batteryTemperature = 0
-                    
-                    print(startupStatus)
-                    
-                    gm.setSpeed(speed)
-                    gm.setBatteryLevel(soc)
-                    gm.setBatteryTemperature(batteryTemperature)
-                    gm.setShutdown(vcuError)
-                    gm.setMotorTemperatureOne(leftFrontMotorTemperature)
-                    gm.setMotorTemperatureTwo(rightFrontMotorTemperature)
-                    gm.setMotorTemperatureThree(leftRearMotorTemperature)
-                    gm.setMotorTemperatureFour(rightRearMotorTemperature)
-                    gm.setHV(0)
-                    gm.setLV(0)
-                    gm.setMaxPower(maxPower)
-                    
-                    #print("Done")
+        if int(hex(message.arbitration_id)[2:]) == temperatureID:
+            leftFrontMotorTemperature = message.data[0] - 20
+            leftRearMotorTemperature = message.data[1] - 20
+            rightFrontMotorTemperature = message.data[2] - 20
+            rightRearMotorTemperature = message.data[3] - 20
+            batteryTemperature = message.data[4] - 20
+        
+        if int(hex(message.arbritation_id[2:])) == vehicleID:
+            maxPower = message.data[0] 
+            soc = message.data[1]
+            shutdown = message.data[2]
+            speed = message.data[3]
+            startupStatus = message.data[4]
+
+        gm.setSpeed(speed)
+        gm.setBatteryLevel(soc)
+        gm.setBatteryTemperature(batteryTemperature)
+        gm.setShutdown(shutdown)
+        gm.setMotorTemperatureOne(leftFrontMotorTemperature)
+        gm.setMotorTemperatureTwo(rightFrontMotorTemperature)
+        gm.setMotorTemperatureThree(leftRearMotorTemperature)
+        gm.setMotorTemperatureFour(rightRearMotorTemperature)
+        gm.setStartupStatus(startupStatus)
+        gm.setMaxPower(maxPower)
+       
 
 except KeyboardInterrupt:
 	#Catch keyboard interrupt
